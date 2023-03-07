a04c28ef2eb65176127602890e214d6f80ff6348
RainyArcher <74546799+RainyArcher@users.noreply.github.com>
2023-03-07 14:11:25 +0300

    Added InvalidURL exception handling, fixed bugs

--- VerseAPI.py
+++ VerseAPI.py
@@ -4,16 +4,20 @@ from data.Verse import Verse
 
 class WebScaper():
     def Search(query):
-        request = requests.get(
+        try:
+            request = requests.get(
             f'https://ilibrary.ru/text/{query}/p.1/index.html')
-        soup = BeautifulSoup(request.content, 'html.parser')
-        fullTitle = str(soup.find('title'))[7:-8]
-        if fullTitle:
-            title = fullTitle[fullTitle.find("«"):fullTitle.find("»")]
-            author = fullTitle[:fullTitle.find("«")]
-            text = []
-            for string in soup.find_all('span', class_="vl"):
-                text.append(str(string).split('<span class="vl"><span class="i1"></span>')[1].split('<span class="i2">')[0])
-            return Verse(author, title, text)
-        else:
-            return "Простите, но такого стихотворения нет в базе."
+            soup = BeautifulSoup(request.content, 'html.parser')
+            fullTitle = str(soup.find('title'))[7:-8]
+            if fullTitle:
+                author = '. '.join(fullTitle.split('. ')[0:3])
+                title = '. '.join(fullTitle.split('. ')[3:-1])
+                text = []
+                for string in soup.find_all('span', class_="vl"):
+                    text.append(str(string).split('<span class="i1"></span>')[1].split('<span class="i2">')[0].strip())
+                return Verse(author, title, text)
+            else:
+                return "Простите, но такого стихотворения нет в базе."
+        except requests.exceptions.InvalidURL:
+            print("Неверно указан адрес сервера")
+
